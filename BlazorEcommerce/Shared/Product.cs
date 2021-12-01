b427e54e402918b7daf503e1e7afe3f7637a7f76
Your Name <you@example.com>
2021-12-01 15:43:33 +0700

    add productList component

---
+++ BlazorEcommerce/Shared/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Decription { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
